4d0b9378419c3c02623748c6c1668b62f21eb624
dclxxxvi <t.aleksandr888@mail.ru>
2021-12-08 01:42:53 +0500

    Выполнено задание "Корреляция" по работе с MediaWiki Action API

--- wikiAPI_task.py
+++ wikiAPI_task.py
@@ -11,6 +11,14 @@ def main():
           '%D1%87 '
     get_statistics(url, '183903', 'revisions.txt')
 
+    #Наибольшее число правок было сделано в день смерти.
+
+    url1 = 'https://ru.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&rvlimit=500&titles=%D0%91%D0' \
+           '%B5%D0%BB%D1%8C%D0%BC%D0%BE%D0%BD%D0%B4%D0%BE,_%D0%96%D0%B0%D0%BD-%D0%9F%D0%BE%D0%BB%D1%8C '
+    get_statistics(url1, '192203', 'revisions1.txt')
+
+    #Наибольшее число правок было 2021-09-06 числа и составляет 58 правок. В эту же дату писатель погиб.
+    #Такая метрика имеет место быть, но не абсолютна достоверна.
 
 def get_statistics(url, pageid, filename):
     data = loads(urlopen(url).read().decode('utf8'))
