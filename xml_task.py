72e2b71e791e3dff4233e37b34b4568ae166f90a
dclxxxvi <t.aleksandr888@mail.ru>
2021-12-08 01:15:49 +0500

    Выполнены все задания из работы с xml

--- xml_task.py
+++ xml_task.py
@@ -9,6 +9,7 @@ def main():
     items = root.find('channel').findall('item')
     tags_to_find = ['pubDate', 'title']
     parse_tags(items, tags_to_find, 'news.json')
+    parse_items(items, 'news1.json')
 
 
 def parse_tags(items, tags_to_find, filename):
@@ -16,6 +17,19 @@ def parse_tags(items, tags_to_find, filename):
     for item in items:
         for tag in tags_to_find:
             result.append({tag: item.find(tag).text})
+    with open(filename, 'w', encoding='utf8') as file:
+        json.dump(result, file, indent=1, ensure_ascii=False)
+
+
+def parse_items(items, filename):
+    result = []
+    for item in items:
+        data = {}
+        for i in item.iter():
+            if i == item:
+                continue
+            data[i.tag] = i.text
+        result.append(data)
     with open(filename, 'w', encoding='utf8') as file:
         json.dump(result, file, indent=1, ensure_ascii=False)
 
